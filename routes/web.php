d9b5c0a1155222f24db651fbd070d5c3c20dfd1f
Alexis <alexisburgos1997@gmail.com>
2022-11-11 01:22:55 -0300

    Roles usuario

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
-Route::group(['prefix' => 'dashboard', 'middleware' => 'auth'], function(){
+Route::group(['prefix' => 'dashboard', 'middleware' => ['auth', 'admin'] ], function(){
 
     Route::get('/', function () {
         return view('dashboard');
